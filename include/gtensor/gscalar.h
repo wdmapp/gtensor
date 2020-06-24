240d27fd15a72af79191a79ae0a86412a3eaa92a
Bryce Allen <bdallen@uchicago.edu>
2020-06-23 20:15:32 -0400

    gscalar: store decayed value type
    This fixes some confusing edge cases. Always copying the value
    should not cause any performance issue, given that all scalars
    in known use are small.

--- include/gtensor/gscalar.h
+++ include/gtensor/gscalar.h
@@ -18,7 +18,7 @@ public:
 
   constexpr static size_type dimension() { return 0; }
 
-  gscalar(T&& value) : value_(value) {}
+  gscalar(T value) : value_(value) {}
 
   shape_type shape() const { return {}; }
 
@@ -28,19 +28,16 @@ public:
     return value_;
   }
 
-  gscalar<value_type> to_kernel() const
-  {
-    return gscalar<value_type>(value_type(value_));
-  }
+  gscalar<value_type> to_kernel() const { return gscalar<value_type>(value_); }
 
 private:
-  T value_; // FIXME? would be much less fragile to never store reference
+  value_type value_;
 };
 
 template <typename T>
-gscalar<T> scalar(T&& t)
+gscalar<T> scalar(T t)
 {
-  return gscalar<T>(std::forward<T>(t));
+  return gscalar<T>(t);
 }
 
 } // namespace gt
