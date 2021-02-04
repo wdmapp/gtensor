be64fbdac0d818fcee3325ed48c2d3fc7b167630
Bryce Allen <bdallen@uchicago.edu>
2021-02-03 20:26:03 -0500

    tests: sarray fix for device, add assign tests

--- tests/test_sarray.cxx
+++ tests/test_sarray.cxx
@@ -22,7 +22,7 @@ void test_launch_insert()
     g3.shape(), GT_LAMBDA(int i) {
       auto a2 = a.insert(0, -1);
       auto a3 = a2.insert(6, 5);
-      if (i < g2.shape(0))
+      if (i < k2.shape(0))
         k2(i) = a2[i];
       k3(i) = a3[i];
     });
@@ -50,7 +50,7 @@ void test_launch_remove()
       auto a2 = a.remove(0);
       auto a3 = a2.remove(3);
       k2(i) = a2[i];
-      if (i < g3.shape(0))
+      if (i < k3.shape(0))
         k3(i) = a3[i];
     });
 
@@ -58,6 +58,28 @@ void test_launch_remove()
   EXPECT_EQ(h2, (gt::gtensor<int, 1>{1, 2, 3, 4}));
   gt::copy(g3, h3);
   EXPECT_EQ(h3, (gt::gtensor<int, 1>{1, 2, 3}));
+}
+
+template <typename S>
+void test_launch_assign()
+{
+  gt::sarray<int, 5> a(0, 1, 2, 3, 4);
+  gt::gtensor<int, 1, S> g2(gt::shape(5));
+  gt::gtensor<int, 1> h2(gt::shape(5));
+
+  auto k2 = g2.to_kernel();
+
+  gt::launch<1, S>(
+    g2.shape(), GT_LAMBDA(int i) {
+      gt::sarray<int, 5> a2;
+      for (int j = 0; j < 5; j++) {
+        a2[j] = 2 * a[j];
+      }
+      k2(i) = a2[i];
+    });
+
+  gt::copy(g2, h2);
+  EXPECT_EQ(h2, (gt::gtensor<int, 1>{0, 2, 4, 6, 8}));
 }
 
 TEST(sarray, insert)
@@ -103,6 +125,11 @@ TEST(sarray, host_launch_remove)
   test_launch_remove<gt::space::host>();
 }
 
+TEST(sarray, host_launch_assign)
+{
+  test_launch_assign<gt::space::host>();
+}
+
 #ifdef GTENSOR_HAVE_DEVICE
 
 TEST(sarray, device_launch_insert)
@@ -113,6 +140,11 @@ TEST(sarray, device_launch_insert)
 TEST(sarray, device_launch_remove)
 {
   test_launch_remove<gt::space::device>();
+}
+
+TEST(sarray, device_launch_assign)
+{
+  test_launch_assign<gt::space::device>();
 }
 
 #endif
