ffcbd005b496dbe84332c164c70d600e5b30d65a
Bryce Allen <bdallen@uchicago.edu>
2021-02-15 11:06:49 -0500

    ci: use custom script for SYCL tests
    Reduction tests are failing on github CI, and work fine on
    my laptop. The cmake test output doesn't show details of the
    failure, so use modified custom run script instead.

--- run-gtests.sh
+++ run-gtests.sh
@@ -6,8 +6,12 @@ for f in "$build_dir"/tests/test_*; do
     if [ -x "$f" ]; then
         test_dir=$(dirname "$f")
         test_name=$(basename "$f")
-        $f >$test_dir/$test_name.log 2>&1
+        test_log=$test_dir/$test_name.log
+        $f >$test_log 2>&1
         result=$?
         echo $test_name $result
+        if [ $result -ne 0 ]; then
+          cat $test_log
+        fi
     fi
 done
