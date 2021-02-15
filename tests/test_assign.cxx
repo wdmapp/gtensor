588006e02fd3e454af019d4be9a996dc510086bb
Bryce Allen <bdallen@uchicago.edu>
2021-02-14 21:15:55 -0500

    add fill method for containers, views, and spans
    Standard interface for doing scalar assignment which avoids some
    subtle design dilemas.

--- tests/test_assign.cxx
+++ tests/test_assign.cxx
@@ -20,6 +20,38 @@ TEST(assign, gtensor_6d)
   EXPECT_EQ(a, b);
 }
 
+TEST(assign, gtensor_fill)
+{
+  gt::gtensor<float, 2> a(gt::shape(2, 3));
+
+  a.fill(9001);
+
+  EXPECT_EQ(a,
+            (gt::gtensor<float, 2>{{9001, 9001}, {9001, 9001}, {9001, 9001}}));
+}
+
+TEST(assign, gview_fill)
+{
+  gt::gtensor<float, 2> a(gt::shape(2, 3));
+  auto av = a.view(gt::all, gt::all);
+
+  av.fill(9001);
+
+  EXPECT_EQ(a,
+            (gt::gtensor<float, 2>{{9001, 9001}, {9001, 9001}, {9001, 9001}}));
+}
+
+TEST(assign, span_fill)
+{
+  gt::gtensor<float, 2> a(gt::shape(2, 3));
+  auto as = a.to_kernel();
+
+  as.fill(9001);
+
+  EXPECT_EQ(a,
+            (gt::gtensor<float, 2>{{9001, 9001}, {9001, 9001}, {9001, 9001}}));
+}
+
 #ifdef GTENSOR_HAVE_DEVICE
 
 TEST(assign, device_gtensor_6d)
@@ -40,6 +72,44 @@ TEST(assign, device_gtensor_6d)
   gt::copy(b, h_b);
 
   EXPECT_EQ(h_a, h_b);
+}
+
+TEST(assign, device_gtensor_fill)
+{
+  gt::gtensor_device<float, 2> a(gt::shape(2, 3));
+  gt::gtensor<float, 2> h_a(a.shape());
+
+  a.fill(9001);
+
+  gt::copy(a, h_a);
+  EXPECT_EQ(h_a, (gt::gtensor_device<float, 2>{
+                   {9001, 9001}, {9001, 9001}, {9001, 9001}}));
+}
+
+TEST(assign, device_gview_fill)
+{
+  gt::gtensor_device<float, 2> a(gt::shape(2, 3));
+  gt::gtensor<float, 2> h_a(a.shape());
+  auto av = a.view(gt::all, gt::all);
+
+  av.fill(9001);
+
+  gt::copy(a, h_a);
+  EXPECT_EQ(h_a,
+            (gt::gtensor<float, 2>{{9001, 9001}, {9001, 9001}, {9001, 9001}}));
+}
+
+TEST(assign, device_span_fill)
+{
+  gt::gtensor_device<float, 2> a(gt::shape(2, 3));
+  gt::gtensor<float, 2> h_a(a.shape());
+  auto as = a.to_kernel();
+
+  as.fill(9001);
+
+  gt::copy(a, h_a);
+  EXPECT_EQ(h_a,
+            (gt::gtensor<float, 2>{{9001, 9001}, {9001, 9001}, {9001, 9001}}));
 }
 
 #endif // GTENSOR_HAVE_DEVICE
