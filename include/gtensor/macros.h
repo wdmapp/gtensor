54bc38a11ff83a4b89e17f51ba5f734b42d860e1
Bryce Allen <bdallen@uchicago.edu>
2021-02-24 18:44:15 -0500

    sycl: fix debug builds
    disable bounds check for SYCL even when NDEBUG is not set, because
    the current implementation fails to commpile. SYCL does not allow
    variadic function calls in kernels.

--- include/gtensor/macros.h
+++ include/gtensor/macros.h
@@ -28,7 +28,9 @@
 
 #endif
 
-#ifndef NDEBUG
+// Note: SYCL doesn't allow variadic function calls in kernel code, so the
+// bounds check implementation does not currently work.
+#if !defined(NDEBUG) && !defined(GTENSOR_DEVICE_SYCL)
 #define GT_BOUNDSCHECK
 #endif
 
