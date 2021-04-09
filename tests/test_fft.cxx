082dda4ab3799eeb0c81213606749b05d72941aa
Bryce Allen <bdallen@uchicago.edu>
2021-04-09 15:54:04 -0400

    fft: add 2d and 3d tests with delta fn

--- tests/test_fft.cxx
+++ tests/test_fft.cxx
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <iostream>
 #include <stdexcept>
 
 #include "gtensor/gtensor.h"
@@ -294,4 +295,167 @@ TEST(fft, move_only)
   expect_complex_near(h_B(0, 0), T(8, 0));
   expect_complex_near(h_B(1, 0), T(3, 1));
   expect_complex_near(h_B(2, 0), T(-6, 0));
+}
+
+template <typename E>
+void fft_r2c_2d()
+{
+  constexpr int N = 17;
+  constexpr int batch_size = 1;
+  using T = gt::complex<E>;
+
+  auto h_A = gt::zeros<E>({N, N, batch_size});
+  auto d_A = gt::empty_device<E>(h_A.shape());
+
+  auto h_A2 = gt::empty<E>(h_A.shape());
+  auto d_A2 = gt::empty_device<E>(h_A.shape());
+
+  auto h_B = gt::empty<T>({N, N / 2 + 1, batch_size});
+  auto d_B = gt::empty_device<T>(h_B.shape());
+
+  // origin at center of domain has value 1, to model delta function
+  h_A(8, 8, 0) = 1.0;
+
+  gt::copy(h_A, d_A);
+
+  gt::fft::FFTPlanMany<gt::fft::Domain::REAL, E> plan({N, N}, batch_size);
+  plan(d_A, d_B);
+  gt::copy(d_B, h_B);
+
+  std::cout << "h_A.shape() = " << h_A.shape() << std::endl;
+  std::cout << "h_B.shape() = " << h_B.shape() << std::endl;
+  std::cout << "h_A = " << h_A << std::endl;
+  std::cout << "h_B = " << h_B << std::endl;
+
+  // FFT of delta function is all ones in magnitude
+  auto h_B_flat = gt::flatten(h_B);
+  double max_err = gt::test::detail::max_err<E>::value;
+  for (int i = 0; i < h_B_flat.shape(0); i++) {
+    ASSERT_NEAR(gt::abs(h_B_flat(i)), 1.0, max_err);
+  }
+
+  // test roundtripping data, with normalization
+  plan.inverse(d_B, d_A2);
+  gt::copy(d_A2, h_A2);
+  for (int i = 0; i < h_A.shape(0); i++) {
+    for (int j = 0; j < h_A.shape(1); j++) {
+      ASSERT_NEAR(h_A(i, j, 0), h_A2(i, j, 0) / (N * N), max_err);
+    }
+  }
+}
+
+TEST(fft, r2c_2d)
+{
+  fft_r2c_2d<float>();
+}
+
+TEST(fft, d2z_2d)
+{
+  fft_r2c_2d<double>();
+}
+
+template <typename E>
+void fft_c2c_2d()
+{
+  constexpr int N = 17;
+  constexpr int batch_size = 1;
+  using T = gt::complex<E>;
+
+  auto h_A = gt::zeros<T>({N, N, batch_size});
+  auto d_A = gt::empty_device<T>(h_A.shape());
+
+  auto h_A2 = gt::empty<T>(h_A.shape());
+  auto d_A2 = gt::empty_device<T>(h_A.shape());
+
+  auto h_B = gt::empty<T>({N, N, batch_size});
+  auto d_B = gt::empty_device<T>(h_B.shape());
+
+  // origin at center of domain has value 1, to model delta function
+  h_A(8, 8, 0) = T(1.0, 0.0);
+
+  gt::copy(h_A, d_A);
+
+  gt::fft::FFTPlanMany<gt::fft::Domain::COMPLEX, E> plan({N, N}, batch_size);
+  plan(d_A, d_B);
+  gt::copy(d_B, h_B);
+
+  // FFT of delta function is all ones in magnitude
+  auto h_B_flat = gt::flatten(h_B);
+  double max_err = gt::test::detail::max_err<E>::value;
+  for (int i = 0; i < h_B_flat.shape(0); i++) {
+    ASSERT_NEAR(gt::abs(h_B_flat(i)), 1.0, max_err);
+  }
+
+  // test roundtripping data, with normalization
+  plan.inverse(d_B, d_A2);
+  gt::copy(d_A2, h_A2);
+  for (int i = 0; i < h_A.shape(0); i++) {
+    for (int j = 0; j < h_A.shape(1); j++) {
+      expect_complex_near(h_A(i, j, 0), h_A2(i, j, 0) / T(N * N, 0));
+    }
+  }
+}
+
+TEST(fft, c2c_2d)
+{
+  fft_c2c_2d<float>();
+}
+
+TEST(fft, z2z_2d)
+{
+  fft_c2c_2d<double>();
+}
+
+template <typename E>
+void fft_r2c_3d()
+{
+  constexpr int N = 17;
+  constexpr int batch_size = 1;
+  using T = gt::complex<E>;
+
+  auto h_A = gt::zeros<E>({N, N, N, batch_size});
+  auto d_A = gt::empty_device<E>(h_A.shape());
+
+  auto h_A2 = gt::empty<E>(h_A.shape());
+  auto d_A2 = gt::empty_device<E>(h_A.shape());
+
+  auto h_B = gt::empty<T>({N, N, N / 2 + 1, batch_size});
+  auto d_B = gt::empty_device<T>(h_B.shape());
+
+  // origin at center of domain has value 1, to model delta function
+  h_A(8, 8, 8, 0) = 1.0;
+
+  gt::copy(h_A, d_A);
+
+  gt::fft::FFTPlanMany<gt::fft::Domain::REAL, E> plan({N, N, N}, batch_size);
+  plan(d_A, d_B);
+  gt::copy(d_B, h_B);
+
+  // FFT of delta function is all ones in magnitude
+  auto h_B_flat = gt::flatten(h_B);
+  double max_err = gt::test::detail::max_err<E>::value;
+  for (int i = 0; i < h_B_flat.shape(0); i++) {
+    ASSERT_NEAR(gt::abs(h_B_flat(i)), 1.0, max_err);
+  }
+
+  // test roundtripping data, with normalization
+  plan.inverse(d_B, d_A2);
+  gt::copy(d_A2, h_A2);
+  for (int i = 0; i < h_A.shape(0); i++) {
+    for (int j = 0; j < h_A.shape(1); j++) {
+      for (int k = 0; k < h_A.shape(2); k++) {
+        ASSERT_NEAR(h_A(i, j, k, 0), h_A2(i, j, k, 0) / (N * N * N), max_err);
+      }
+    }
+  }
+}
+
+TEST(fft, r2c_3d)
+{
+  fft_r2c_3d<float>();
+}
+
+TEST(fft, d2z_3d)
+{
+  fft_r2c_3d<double>();
 }
