b67f4f103541c7653951f58f58aa1a6d76f88ef6
Bryce Allen <bdallen@uchicago.edu>
2021-04-19 11:51:35 -0500

    test: exclude known sycl failures

--- run-gtests.sh
+++ run-gtests.sh
@@ -4,10 +4,18 @@ build_dir=${1:-.}
 
 exit_code=0
 
+exclude=${GTENSOR_TEST_EXCLUDE:-}
+echo "EXCLUDE: $exclude"
+
 for f in "$build_dir"/tests/test_*; do
     if [ -x "$f" ]; then
         test_dir=$(dirname "$f")
         test_name=$(basename "$f")
+        echo $exclude | grep -wq $test_name
+        if [ $? -eq 0 ]; then
+          echo "$test_name excluded"
+          continue
+        fi
         test_log=$test_dir/$test_name.log
         $f >$test_log 2>&1
         result=$?
