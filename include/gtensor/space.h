0f9fb83d2f440f399d597d28a76ccb8ff9c1a7ec
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-24 09:41:07 -0400

    allocator: mv device_allocator into gt::allocator ns

--- include/gtensor/space.h
+++ include/gtensor/space.h
@@ -128,6 +128,24 @@ inline bool operator!=(const caching_allocator<T, AT>& a,
   return !(a == b);
 }
 
+namespace allocator
+{
+#ifdef GTENSOR_HAVE_DEVICE
+#ifdef GTENSOR_USE_THRUST
+
+#if GTENSOR_DEVICE_CUDA && THRUST_VERSION <= 100903
+template <typename T>
+using device_allocator =
+  caching_allocator<T, thrust::device_malloc_allocator<T>>;
+#else
+template <typename T>
+using device_allocator = caching_allocator<T, thrust::device_allocator<T>>;
+#endif
+
+#endif
+#endif
+} // namespace allocator
+
 // ======================================================================
 // space
 
@@ -154,19 +172,10 @@ struct host
 
 #ifdef GTENSOR_USE_THRUST
 
-#if GTENSOR_DEVICE_CUDA && THRUST_VERSION <= 100903
-template <typename T>
-using device_allocator =
-  caching_allocator<T, thrust::device_malloc_allocator<T>>;
-#else
-template <typename T>
-using device_allocator = caching_allocator<T, thrust::device_allocator<T>>;
-#endif
-
 struct device
 {
   template <typename T>
-  using Vector = thrust::device_vector<T, device_allocator<T>>;
+  using Vector = thrust::device_vector<T, gt::allocator::device_allocator<T>>;
   template <typename T>
   using Span = device_span<T>;
 };
