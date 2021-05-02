e6c14b592c7b14db4cf72407e9e2fb52d5c16c77
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-05-02 00:50:00 -0400

    copy: add space_type to pointer_traits

--- include/gtensor/pointer_traits.h
+++ include/gtensor/pointer_traits.h
@@ -3,6 +3,8 @@
 #define POINTER_TRAITS_H
 
 #include "device_ptr.h"
+#include "space_forward.h"
+
 #ifdef GTENSOR_USE_THRUST
 #include <thrust/device_ptr.h>
 #endif
@@ -21,6 +23,7 @@ struct pointer_traits<T*>
   using const_pointer = const T*;
   using reference = T&;
   using const_reference = const T&;
+  using space_type = gt::space::host;
 
   template <typename U>
   using rebind = U*;
@@ -36,6 +39,7 @@ struct pointer_traits<gt::device_ptr<T>>
   using const_pointer = gt::device_ptr<const T>;
   using reference = T&;
   using const_reference = const T&;
+  using space_type = gt::space::device;
 
   template <typename U>
   using rebind = gt::device_ptr<U>;
@@ -53,6 +57,7 @@ struct pointer_traits<::thrust::device_ptr<T>>
   using const_pointer = ::thrust::device_ptr<const T>;
   using reference = ::thrust::device_reference<T>;
   using const_reference = ::thrust::device_reference<const T>;
+  using space_type = gt::space::thrust;
 
   template <typename U>
   using rebind = ::thrust::device_ptr<U>;
