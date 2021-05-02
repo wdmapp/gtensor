7056eac5c1ab82dc079ebdf846e810d77b6c47f2
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-05-02 00:49:39 -0400

    copy: mv space definitions to space_forward.h
    Eventually, hopefully the current space.h can go away, and this can
    be renamed to space.h

--- include/gtensor/device_backend.h
+++ include/gtensor/device_backend.h
@@ -28,49 +28,12 @@
 #include "defs.h"
 #include "macros.h"
 #include "pointer_traits.h"
+#include "space_forward.h"
 
 namespace gt
 {
-
 namespace space
 {
-
-struct host
-{};
-#ifdef GTENSOR_USE_THRUST
-struct thrust
-{};
-#endif
-#ifdef GTENSOR_DEVICE_CUDA
-struct cuda
-{};
-#endif
-#ifdef GTENSOR_DEVICE_HIP
-struct hip
-{};
-#endif
-#ifdef GTENSOR_DEVICE_SYCL
-struct sycl
-{};
-#endif
-
-#ifdef GTENSOR_HAVE_DEVICE
-
-#if GTENSOR_USE_THRUST
-using device = thrust;
-#elif GTENSOR_DEVICE_CUDA
-using device = cuda;
-#elif GTENSOR_DEVICE_HIP
-using device = hip;
-#elif GTENSOR_DEVICE_SYCL
-using device = sycl;
-#endif
-
-#else // !  GTENSOR_HAVE_DEVICE
-
-using device = host;
-
-#endif
 
 // ======================================================================
 // space_traits
