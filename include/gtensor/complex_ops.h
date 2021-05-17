31d54aa421f03dd9c21a1720e57335106060e12c
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-05-16 21:58:12 -0400

    func: add gt::exp for scalars, and test it

--- include/gtensor/complex_ops.h
+++ include/gtensor/complex_ops.h
@@ -45,6 +45,15 @@ GT_INLINE complex<T> conj(thrust::device_reference<complex<T>> a)
   return thrust::conj(thrust::raw_reference_cast(a));
 }
 
+using std::exp;
+using thrust::exp;
+
+template <typename T>
+GT_INLINE complex<T> exp(thrust::device_reference<const thrust::complex<T>> a)
+{
+  return thrust::exp(thrust::raw_reference_cast(a));
+}
+
 #else // not CUDA and GTENSOR_USE_THRUST not defined
 
 template <typename T>
@@ -64,6 +73,8 @@ GT_INLINE complex<T> conj(const complex<T>& a)
 {
   return std::conj(a);
 }
+
+using std::exp;
 
 #endif
 
