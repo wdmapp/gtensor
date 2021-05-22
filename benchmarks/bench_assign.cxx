5344c613e15b64e41c8fcd2df4f58dc419833ed5
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-05-22 10:46:27 -0400

    bench: introduce real_t, complex_t

--- benchmarks/bench_assign.cxx
+++ benchmarks/bench_assign.cxx
@@ -3,9 +3,17 @@
 
 #include <gtensor/gtensor.h>
 
+using namespace gt::placeholders;
+
+using real_t = double;
+using complex_t = gt::complex<double>;
+
+// ======================================================================
+// BM_device_assign_4d
+
 static void BM_device_assign_4d(benchmark::State& state)
 {
-  auto a = gt::zeros_device<double>(gt::shape(100, 100, 100, 100));
+  auto a = gt::zeros_device<real_t>(gt::shape(10, 10, 10, 10));
   auto b = gt::empty_like(a);
 
   for (auto _ : state) {
