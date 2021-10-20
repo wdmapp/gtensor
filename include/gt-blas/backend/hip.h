a91af3dddf5800e3b9ed6462ea84107d71dd09da
Gabriele Merlo <merlo@login2.spock.olcf.ornl.gov>
2021-10-20 12:07:32 -0400

    clang format on headers

--- include/gt-blas/backend/hip.h
+++ include/gt-blas/backend/hip.h
@@ -257,7 +257,7 @@ inline void getrf_batched(handle_t* h, int n, T** d_Aarray, int lda,
                                     int lda, gt::blas::index_t* d_PivotArray,  \
                                     int* d_infoArray, int batchSize)           \
   {                                                                            \
-  gtBlasCheck(METHOD(h->handle, n, n,					       \
+    gtBlasCheck(METHOD(h->handle, n, n,                                        \
                        reinterpret_cast<BLASTYPE**>(d_Aarray), lda,            \
                        d_PivotArray, n, d_infoArray, batchSize));              \
   }
@@ -304,23 +304,23 @@ template <typename T>
 inline void getrf_npvt_batched(handle_t* h, int n, T** d_Aarray, int lda,
                                int* d_infoArray, int batchSize);
 
-#define CREATE_GETRF_NPVT_BATCHED(METHOD, GTTYPE, BLASTYPE)                      \
-  template <>                                                                    \
-  inline void getrf_npvt_batched<GTTYPE>(handle_t * h, int n, GTTYPE** d_Aarray, \
-                                    int lda, int* d_infoArray, int batchSize)    \
-  {                                                                              \
-  gtBlasCheck(METHOD(h->handle, n, n,					         \
-                       reinterpret_cast<BLASTYPE**>(d_Aarray), lda,              \
-                       d_infoArray, batchSize));                                 \
+#define CREATE_GETRF_NPVT_BATCHED(METHOD, GTTYPE, BLASTYPE)                    \
+  template <>                                                                  \
+  inline void getrf_npvt_batched<GTTYPE>(handle_t * h, int n,                  \
+                                         GTTYPE** d_Aarray, int lda,           \
+                                         int* d_infoArray, int batchSize)      \
+  {                                                                            \
+    gtBlasCheck(METHOD(h->handle, n, n,                                        \
+                       reinterpret_cast<BLASTYPE**>(d_Aarray), lda,            \
+                       d_infoArray, batchSize));                               \
   }
 
 CREATE_GETRF_NPVT_BATCHED(rocsolver_zgetrf_npvt_batched, gt::complex<double>,
-                     rocblas_double_complex)
+                          rocblas_double_complex)
 CREATE_GETRF_NPVT_BATCHED(rocsolver_cgetrf_npvt_batched, gt::complex<float>,
-                     rocblas_float_complex)
+                          rocblas_float_complex)
 
 #undef CREATE_GETRF_NPVT_BATCHED
-
 
 } // namespace blas
 
