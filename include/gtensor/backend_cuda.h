a4424afc0f9cb496eaaa1edde85565bba166b205
Bryce Allen <bdallen@uchicago.edu>
2023-03-17 09:44:57 -0500

    cuda: add option to use rapids RMM pool allocator
    cmake option GTENSOR_USE_RMM

--- include/gtensor/backend_cuda.h
+++ include/gtensor/backend_cuda.h
@@ -6,8 +6,17 @@
 
 #include <cuda_runtime_api.h>
 
-//#include "thrust/cuda/system/execution_policy.h"
+// #include "thrust/cuda/system/execution_policy.h"
 #include "thrust/execution_policy.h"
+
+#ifdef GTENSOR_USE_RMM
+#include <map>
+
+#include <rmm/mr/device/cuda_memory_resource.hpp>
+#include <rmm/mr/device/managed_memory_resource.hpp>
+#include <rmm/mr/device/pool_memory_resource.hpp>
+#include <rmm/mr/host/pinned_memory_resource.hpp>
+#endif
 
 // ======================================================================
 // gt::backend::cuda
@@ -19,6 +28,160 @@ namespace backend
 
 namespace allocator_impl
 {
+
+#ifdef GTENSOR_USE_RMM
+
+class memory_resources
+{
+public:
+  memory_resources()
+    : mr_host_{},
+      mr_cuda_device_{},
+      mr_cuda_managed_{},
+      mr_device_{&mr_cuda_device_},
+      mr_managed_{&mr_cuda_managed_},
+      allocated_{}
+  {}
+
+  void* allocate_host(size_type nbytes)
+  {
+    void* p = mr_host_.allocate(nbytes);
+    if (p == nullptr) {
+      throw std::runtime_error("host allocate failed");
+    }
+    allocated_.emplace(std::make_pair(p, nbytes));
+    return p;
+  }
+
+  void deallocate_host(void* p)
+  {
+    assert(p != nullptr);
+    auto it = allocated_.find(p);
+    assert(it != allocated_.end());
+    mr_host_.deallocate(p, it->second);
+    allocated_.erase(it);
+  }
+
+  void* allocate_device(size_type nbytes)
+  {
+    void* p = mr_device_.allocate(nbytes);
+    if (p == nullptr) {
+      throw std::runtime_error("device allocate failed");
+    }
+    allocated_.emplace(std::make_pair(p, nbytes));
+    return p;
+  }
+
+  void deallocate_device(void* p)
+  {
+    assert(p != nullptr);
+    auto it = allocated_.find(p);
+    assert(it != allocated_.end());
+    mr_device_.deallocate(p, it->second);
+    allocated_.erase(it);
+  }
+
+  void* allocate_managed(size_type nbytes)
+  {
+    void* p = mr_managed_.allocate(nbytes);
+    if (p == nullptr) {
+      throw std::runtime_error("managed allocate failed");
+    }
+    allocated_.emplace(std::make_pair(p, nbytes));
+    return p;
+  }
+
+  void deallocate_managed(void* p)
+  {
+    assert(p != nullptr);
+    auto it = allocated_.find(p);
+    assert(it != allocated_.end());
+    mr_managed_.deallocate(p, it->second);
+    allocated_.erase(it);
+  }
+
+private:
+  rmm::mr::pinned_memory_resource mr_host_;
+  rmm::mr::cuda_memory_resource mr_cuda_device_;
+  rmm::mr::managed_memory_resource mr_cuda_managed_;
+  rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource> mr_device_;
+  rmm::mr::pool_memory_resource<rmm::mr::managed_memory_resource> mr_managed_;
+
+  std::map<void*, size_type> allocated_;
+};
+
+inline memory_resources& rmm_get_mr_instance()
+{
+  static memory_resources mr;
+  return mr;
+}
+
+template <>
+struct gallocator<gt::space::cuda>
+{
+  template <typename T>
+  static T* allocate(size_type n)
+  {
+    auto nbytes = sizeof(T) * n;
+    return static_cast<T*>(rmm_get_mr_instance().allocate_device(nbytes));
+  }
+
+  template <typename T>
+  static void deallocate(T* p)
+  {
+    rmm_get_mr_instance().deallocate_device(p);
+  }
+};
+
+template <>
+struct gallocator<gt::space::cuda_managed>
+{
+  template <typename T>
+  static T* allocate(size_type n)
+  {
+    auto nbytes = sizeof(T) * n;
+    auto mtype = gt::backend::get_managed_memory_type();
+    if (mtype == gt::backend::managed_memory_type::managed) {
+      return static_cast<T*>(rmm_get_mr_instance().allocate_managed(nbytes));
+    } else if (mtype == gt::backend::managed_memory_type::device) {
+      return static_cast<T*>(rmm_get_mr_instance().allocate_device(nbytes));
+    } else {
+      throw std::runtime_error("unsupported managed memory type for backend");
+    }
+  }
+
+  template <typename T>
+  static void deallocate(T* p)
+  {
+    auto mtype = gt::backend::get_managed_memory_type();
+    if (mtype == gt::backend::managed_memory_type::managed) {
+      rmm_get_mr_instance().deallocate_managed(p);
+    } else if (mtype == gt::backend::managed_memory_type::device) {
+      rmm_get_mr_instance().deallocate_device(p);
+    } else {
+      throw std::runtime_error("unsupported managed memory type for backend");
+    }
+  }
+};
+
+template <>
+struct gallocator<gt::space::cuda_host>
+{
+  template <typename T>
+  static T* allocate(size_type n)
+  {
+    auto nbytes = sizeof(T) * n;
+    return static_cast<T*>(rmm_get_mr_instance().allocate_host(nbytes));
+  }
+
+  template <typename T>
+  static void deallocate(T* p)
+  {
+    rmm_get_mr_instance().deallocate_host(p);
+  }
+};
+
+#else // GTENSOR_USE_RMM
 
 template <>
 struct gallocator<gt::space::cuda>
@@ -42,7 +205,7 @@ template <>
 struct gallocator<gt::space::cuda_managed>
 {
   template <typename T>
-  static T* allocate(size_t n)
+  static T* allocate(size_type n)
   {
     T* p;
     auto nbytes = sizeof(T) * n;
@@ -81,6 +244,8 @@ struct gallocator<gt::space::cuda_host>
     gtGpuCheck(cudaFreeHost(p));
   }
 };
+
+#endif // GTENSOR_USE_RMM
 
 } // namespace allocator_impl
 
