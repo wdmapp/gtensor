e26f092e16181e7a7d27c640a22982662e537ede
Carl-Martin Pfeiler <carl-martin.pfeiler@ipp.mpg.de>
2023-08-17 18:02:35 +0200

    [clean] Namespace and renaming

--- include/gtensor/half.h
+++ include/gtensor/half.h
@@ -1,46 +1,52 @@
-// half_wrapper.hxx
-#ifndef HALF_WRAPPER
-#define HALF_WRAPPER
+#ifndef GTENSOR_HALF_H
+#define GTENSOR_HALF_H
 
 #include <iostream>
 #include <cuda_fp16.h>
 
+namespace gt
+{
+
+// ======================================================================
+// half
 #if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ >= 530)
 #define TARGET_ARCH __host__ __device__
 #else
 #define TARGET_ARCH
 #endif
 
-class HalfWrapper
+class half
 {
 public:
-    TARGET_ARCH HalfWrapper(float x) : x(x) {}; 
-    TARGET_ARCH HalfWrapper(half x) : x(x) {}; 
-    TARGET_ARCH const HalfWrapper& operator=(const float f) { x = f; return *this; }
-    TARGET_ARCH const half& Get() const { return x; }
+    TARGET_ARCH half(float x) : x(x) {};
+    TARGET_ARCH half(__half x) : x(x) {};
+    TARGET_ARCH const half& operator=(const float f) { x = f; return *this; }
+    TARGET_ARCH const __half& Get() const { return x; }
 private:
-    half x;
+    __half x;
 };
 
-TARGET_ARCH const HalfWrapper operator+(const HalfWrapper& lhs, const HalfWrapper& rhs)
+TARGET_ARCH const half operator+(const half& lhs, const half& rhs)
 {
 #if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ >= 530)
-return HalfWrapper( lhs.Get() + rhs.Get() );
+    return half( lhs.Get() + rhs.Get() );
 #else
-return HalfWrapper( float(lhs.Get()) + float(rhs.Get()) );
+    return half( float(lhs.Get()) + float(rhs.Get()) );
 #endif
 }
 
-TARGET_ARCH const HalfWrapper operator*(const HalfWrapper& lhs, const HalfWrapper& rhs)
+TARGET_ARCH const half operator*(const half& lhs, const half& rhs)
 {
 #if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ >= 530)
-return HalfWrapper( lhs.Get() * rhs.Get() );
+    return half( lhs.Get() * rhs.Get() );
 #else
-return HalfWrapper( float(lhs.Get()) * float(rhs.Get()) );
+    return half( float(lhs.Get()) * float(rhs.Get()) );
 #endif
 }
 
-std::ostream& operator<<(std::ostream& s, const HalfWrapper& h)
+std::ostream& operator<<(std::ostream& s, const half& h)
 { s << (float) h.Get(); return s; }
+
+} // namespace gt
 
 #endif
