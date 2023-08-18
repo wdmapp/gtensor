4d2852a63b2147d261b8d4c6a4aa5eca8d818df6
Carl-Martin Pfeiler <carl-martin.pfeiler@ipp.mpg.de>
2023-08-18 15:37:21 +0200

    [test] Explicit kernel haxpy on host + device
        + NOTE: Need to use cmake with CMAKE_CUDA_ARCHITECTURES flag.
                E.g., -DCMAKE_CUDA_ARCHITECTURES=80 for A100
                [some value >= 53 to properly use CUDA __half type]
        + TODO: Later maybe this can be taken care of in CMakeLists.txt

--- tests/test_half.cxx
+++ tests/test_half.cxx
@@ -47,3 +47,57 @@ TEST(half, AutoInitHost)
 
     EXPECT_EQ(a, b);
 }
+
+void host_explicit_haxpy_1d(const gt::half& a,
+                            const gt::gtensor<gt::half, 1>& x,
+                            gt::gtensor<gt::half, 1>& y)
+{
+  auto k_x = x.to_kernel();
+  auto k_y = y.to_kernel();
+
+  gt::launch_host<1>(
+    y.shape(), GT_LAMBDA(int i) { k_y(i) = k_y(i) + a * k_x(i); });
+}
+
+TEST(half, HaxpyExplicit1dHost)
+{
+    gt::gtensor<gt::half, 1, gt::space::host> x(gt::shape(3), 1.5);
+    gt::gtensor<gt::half, 1, gt::space::host> y(x.shape(), 2.5);
+    gt::half a{0.5};
+    gt::gtensor<gt::half, 1, gt::space::host> ref(x.shape(), 3.25);
+
+    host_explicit_haxpy_1d(a, x, y);
+
+    EXPECT_EQ(y, ref);
+}
+
+template <typename S>
+void generic_explicit_haxpy_1d( const gt::half& a,
+                                const gt::gtensor<gt::half, 1, S>& x,
+                                      gt::gtensor<gt::half, 1, S>& y)
+{
+  auto k_x = x.to_kernel();
+  auto k_y = y.to_kernel();
+
+  gt::launch<1, S>(
+    y.shape(), GT_LAMBDA(int i) { k_y(i) = k_y(i) + a * k_x(i); });
+}
+
+TEST(half, HaxpyExplicit1dDevice)
+{
+    gt::gtensor<gt::half, 1, gt::space::host> h_x(gt::shape(3), gt::half{1.5});
+    gt::gtensor<gt::half, 1, gt::space::host> h_y(h_x.shape(), gt::half{2.5});
+    gt::half a{0.5};
+    gt::gtensor<gt::half, 1, gt::space::host> ref(h_x.shape(), 3.25);
+
+    gt::gtensor<gt::half, 1, gt::space::device> d_x(h_x.shape());
+    gt::gtensor<gt::half, 1, gt::space::device> d_y(h_y.shape());
+    gt::copy(h_x, d_x);
+    gt::copy(h_y, d_y);
+
+    generic_explicit_haxpy_1d<gt::space::device>(a, d_x, d_y);
+
+    gt::copy(d_y, h_y);
+
+    EXPECT_EQ(h_y, ref);
+}
