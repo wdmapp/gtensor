7a05b882d2b2160983f58bdada25040fb6a15b53
cmpfeil <carl-martin.pfeiler@ipp.mpg.de>
2023-08-28 13:56:37 +0200

    [fix] Inline gt::half methods/ops via GT_INLINE

--- include/gtensor/half.h
+++ include/gtensor/half.h
@@ -3,17 +3,13 @@
 
 #include <iostream>
 #include <cuda_fp16.h>
+#include <gtensor/macros.h>
 
 namespace gt
 {
 
 // ======================================================================
 // half
-#ifdef __CUDA_ARCH__
-#define TARGET_ARCH __host__ __device__
-#else
-#define TARGET_ARCH
-#endif
 
 #if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ >= 530)
 using compute_type = __half;
@@ -25,25 +21,25 @@ class half
 {
 public:
     half() = default;
-    TARGET_ARCH half(float x) : x(x) {};
-    TARGET_ARCH half(__half x) : x(x) {};
+    GT_INLINE half(float x) : x(x) {};
+    GT_INLINE half(__half x) : x(x) {};
 
-    TARGET_ARCH const half& operator=(const float f) { x = f; return *this; }
-    TARGET_ARCH compute_type Get() const { return static_cast<compute_type>(x); }
+    GT_INLINE const half& operator=(const float f) { x = f; return *this; }
+    GT_INLINE compute_type Get() const { return static_cast<compute_type>(x); }
 private:
     __half x;
 };
 
 #define PROVIDE_HALF_BINARY_ARITHMETIC_OPERATOR(op) \
-    TARGET_ARCH half operator op(const half& lhs, const half& rhs) \
+    GT_INLINE half operator op(const half& lhs, const half& rhs) \
     { return half( lhs.Get() op rhs.Get() ); }
 
 #define PROVIDE_MIXED_HALF_BINARY_ARITHMETIC_OPERATOR(op, fp_type) \
     \
-    TARGET_ARCH fp_type operator op(const half& lhs, const fp_type& rhs) \
+    GT_INLINE fp_type operator op(const half& lhs, const fp_type& rhs) \
     { return static_cast<fp_type>(lhs.Get()) op rhs; } \
     \
-    TARGET_ARCH fp_type operator op(const fp_type& lhs, const half& rhs) \
+    GT_INLINE fp_type operator op(const fp_type& lhs, const half& rhs) \
     { return lhs op static_cast<fp_type>(rhs.Get()); }
 
 PROVIDE_HALF_BINARY_ARITHMETIC_OPERATOR(+);
@@ -62,15 +58,15 @@ PROVIDE_MIXED_HALF_BINARY_ARITHMETIC_OPERATOR(*, double);
 PROVIDE_MIXED_HALF_BINARY_ARITHMETIC_OPERATOR(/, double);
 
 #define PROVIDE_HALF_COMPARISON_OPERATOR(op) \
-    TARGET_ARCH bool operator op(const half& lhs, const half& rhs) \
+    GT_INLINE bool operator op(const half& lhs, const half& rhs) \
     { return lhs.Get() op rhs.Get(); }
 
 #define PROVIDE_MIXED_HALF_COMPARISON_OPERATOR(op, fp_type) \
     \
-    TARGET_ARCH bool operator op(const half& lhs, const fp_type& rhs) \
+    GT_INLINE bool operator op(const half& lhs, const fp_type& rhs) \
     { return static_cast<fp_type>(lhs.Get()) op rhs; } \
     \
-    TARGET_ARCH bool operator op(const fp_type& lhs, const half& rhs) \
+    GT_INLINE bool operator op(const fp_type& lhs, const half& rhs) \
     { return lhs op static_cast<fp_type>(rhs.Get()); }
 
 PROVIDE_HALF_COMPARISON_OPERATOR(==);
@@ -97,7 +93,6 @@ PROVIDE_MIXED_HALF_COMPARISON_OPERATOR(>=, double);
 std::ostream& operator<<(std::ostream& s, const half& h)
 { s << static_cast<float>(h.Get()); return s; }
 
-#undef TARGET_ARCH
 #undef PROVIDE_HALF_BINARY_ARITHMETIC_OPERATOR
 #undef PROVIDE_MIXED_HALF_BINARY_ARITHMETIC_OPERATOR
 #undef PROVIDE_HALF_COMPARISON_OPERATOR
