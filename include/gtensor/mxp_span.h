686e5fcd7085dcb44dd73dffedd48e748b0a9006
Carl-Martin Pfeiler <carl-martin.pfeiler@ipp.mpg.de>
2025-01-22 16:55:02 +0100

    mxp: Most basic test + feature

---
+++ include/gtensor/mxp_span.h
@@ -0,0 +1,60 @@
+#ifndef GTENSOR_MXP_SPAN_H
+#define GTENSOR_MXP_SPAN_H
+
+#include <gtensor/gtensor.h>
+
+// __________________________________________________________________________ //
+
+namespace mxp {
+
+// -------------------------------------------------------------------------- //
+
+template <typename T, gt::size_type N, typename S, typename X>
+class mxp_span : public gt::gtensor_span<T, N, S>,
+                 public gt::expression<mxp_span<T, N, S, X>>
+{
+public:
+  using self_type = mxp_span<T, N, S, X>;
+  using base_type = gt::gtensor_span<T, N, S>;
+  using value_type = X;
+
+  using typename base_type::pointer;
+  using typename base_type::shape_type;
+  using typename base_type::strides_type;
+
+  // ------------------------------------------------------------------------ //
+
+  GT_INLINE mxp_span(pointer data, const shape_type& shape,
+                     const strides_type& strides)
+  : base_type(data, shape, strides) {}
+
+  // ------------------------------------------------------------------------ //
+
+  using base_type::operator=;
+
+  // ------------------------------------------------------------------------ //
+
+  template <typename... Args>
+  GT_INLINE auto operator()(Args&&... args) const -> value_type
+  { return value_type( base_type::operator()(args...) ); }
+
+  // ------------------------------------------------------------------------ //
+
+};
+
+// -------------------------------------------------------------------------- //
+
+template <gt::size_type N, typename X, typename T>
+GT_INLINE auto adapt(T* data, const gt::shape_type<N>& shape)
+{
+  return mxp_span<T, N, gt::space::host, X>(data, shape,
+                                            gt::calc_strides(shape));
+}
+
+// -------------------------------------------------------------------------- //
+
+}; // namespace mxp
+
+// __________________________________________________________________________ //
+
+#endif // GTENSOR_MXP_SPAN_H
---
+++ tests/test_mxp.cxx
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+
+#include <gtensor/gtensor.h>
+
+#include <gtensor/mxp.h>
+
+#include <vector>
+
+TEST(mxp, demo_implicit_axaxaxpy)
+{
+  const int n { 2 };
+  const float x_init { 1.f / 8.f / 1024.f / 1024.f };
+  const float y_init { 1.f };
+  const float a { 1.f / 3.f };
+
+  EXPECT_NE(y_init, y_init + x_init);
+
+  const std::vector<float> x(n, x_init);
+  /* */ std::vector<float> y(n, y_init);
+
+  const auto gt_x = gt::adapt<1>(x.data(), x.size());
+  /* */ auto gt_y = gt::adapt<1>(y.data(), y.size());
+
+  gt_y = gt_y + a * gt_x
+              + a * gt_x
+              + a * gt_x;
+
+  EXPECT_EQ(y[0], y_init);
+  EXPECT_EQ(y[1], y_init);
+
+  const auto mxp_x = mxp::adapt<1, double>(x.data(), x.size());
+  /* */ auto mxp_y = mxp::adapt<1, double>(y.data(), y.size());
+
+  mxp_y = mxp_y + a * mxp_x
+                + a * mxp_x
+                + a * mxp_x;
+
+  EXPECT_EQ(y[0], y_init + x_init);
+  EXPECT_EQ(y[1], y_init + x_init);
+}
