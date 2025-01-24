08d48b23b45e9bf6a01af5eb94ba6a8784d23dcd
Carl-Martin Pfeiler <carl-martin.pfeiler@ipp.mpg.de>
2025-01-24 11:23:00 +0100

    mxp: Failing test for .view(...)

--- tests/test_mxp.cxx
+++ tests/test_mxp.cxx
@@ -518,3 +518,44 @@ TEST(mxp, complex_op_divide_explicit)
 
   EXPECT_LT(mxp_err, 4.0e-08);
 }
+
+TEST(mxp, view_axaxaxpy)
+{
+  const int nx{3};
+  const int ny{5};
+  const float x_init{exp2f(-23)};
+  const float y_init{1.f};
+  const float a{1.f / 3.f};
+
+  EXPECT_NE(y_init, y_init + x_init);
+
+  const std::vector<float> x(nx, x_init);
+  /* */ std::vector<float> y(ny, y_init);
+
+  const auto gt_x = gt::adapt<1>(x.data(), x.size());
+  /* */ auto gt_y = gt::adapt<1>(y.data(), y.size());
+
+  using gt::placeholders::_all;
+  using gt::placeholders::_s;
+
+  gt_y.view(_s(1, -1)) = gt_y.view(_s(1, -1)) + a * gt_x.view(_all) +
+                         a * gt_x.view(_all) + a * gt_x.view(_all);
+
+  EXPECT_EQ(y[0], y_init);
+  EXPECT_EQ(y[1], y_init);
+  EXPECT_EQ(y[2], y_init);
+  EXPECT_EQ(y[3], y_init);
+  EXPECT_EQ(y[4], y_init);
+
+  const auto mxp_x = mxp::adapt<1, double>(x.data(), x.size());
+  /* */ auto mxp_y = mxp::adapt<1, double>(y.data(), y.size());
+
+  mxp_y.view(_s(1, -1)) = mxp_y.view(_s(1, -1)) + a * mxp_x.view(_all) +
+                          a * mxp_x.view(_all) + a * mxp_x.view(_all);
+
+  EXPECT_EQ(y[0], y_init);
+  EXPECT_EQ(y[1], y_init + x_init);
+  EXPECT_EQ(y[2], y_init + x_init);
+  EXPECT_EQ(y[3], y_init + x_init);
+  EXPECT_EQ(y[4], y_init);
+}
